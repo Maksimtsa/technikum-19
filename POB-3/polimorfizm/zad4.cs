2ff7b8ec476f955ae0d82ed156c544db13042552
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2025-10-09 10:09:07 +0200

    Update zad4.cs

--- POB-3/polimorfizm/zad4.cs
+++ POB-3/polimorfizm/zad4.cs
@@ -4,12 +4,18 @@ namespace zad3
 {
     class Vehicle
     {
-        public void Drive() => Console.WriteLine("drive");
+        public void Drive()
+        {
+            Console.WriteLine("drive");
+        }
     }
 
     class Car : Vehicle
     {
-        public new void Drive() => Console.WriteLine("samochód jedzie");
+        public new void Drive()
+        {
+            Console.WriteLine("samochód jedzie");
+        }
     }
 
     internal class Program

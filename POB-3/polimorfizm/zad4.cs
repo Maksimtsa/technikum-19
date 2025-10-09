c2b25f85c9e3952daf2f4f319594663029671bbf
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2025-10-09 10:07:12 +0200

    Create zad4.cs

---
+++ POB-3/polimorfizm/zad4.cs
@@ -0,0 +1,49 @@
+namespace zad3
+{
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double SideA { get; set; }
+
+        public double SideB { get; set; }
+        public Rectangle(double a, double b)
+        {
+            SideA = a;
+            SideB = b;
+        }
+        public override double GetArea()
+        {
+            return SideA * SideB;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shape> Shape = new List<Shape> { new Circle(2), new Rectangle(2, 5) };
+
+            Console.WriteLine(Shape[0].GetArea());
+            Console.WriteLine(Shape[1].GetArea());
+        }
+    }
+}

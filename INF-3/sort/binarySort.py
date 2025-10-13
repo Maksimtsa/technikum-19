5288773be59622656ffca1208dc93300c058eff3
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2025-10-13 13:03:28 +0200

    Create binarySort.py

---
+++ INF-3/sort/binarySort.py
@@ -0,0 +1,23 @@
+import random
+from random import randint, seed
+
+def binary_search(t, e):
+    p = 0
+    k = len(t) - 1
+    while p <= k:
+        sr = (p + k) // 2
+        if t[sr] == e:
+            return sr
+
+        if t[sr] < e:
+            p = sr + 1
+        else:
+            k = sr - 1
+    return -1
+
+seed(3)
+t = [randint(1, 100) for i in range(10)]
+t.sort()
+p = binary_search(t, 9)
+
+print(p)

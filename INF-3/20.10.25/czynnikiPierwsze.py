9a08981dbf42c07f8caa6d159cda8a539fa2f279
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2025-10-20 12:57:15 +0200

    Create czynnikiPierwsze.py

---
+++ INF-3/20.10.25/czynnikiPierwsze.py
@@ -0,0 +1,12 @@
+def czynnikiPierwsze(n):
+    divisior = 2
+    t = []
+    while n > 1:
+        if n % divisior == 0:
+            n //= divisior
+            t.append(divisior)
+        else:
+            divisior += 1
+    return t
+
+print(czynnikiPierwsze(1000))

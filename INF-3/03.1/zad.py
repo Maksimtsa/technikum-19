e889c1c6b3cc4998177ebdb6123b90b79673bebd
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2025-11-03 13:10:44 +0100

    Update zad.py

--- INF-3/03.1/zad.py
+++ INF-3/03.1/zad.py
@@ -11,3 +11,13 @@ with open("zadanie4.txt", 'a') as file:
     file.write(f"zadanie a)\n{count}\n")
 
 #zad b)
+count1 = 0
+count0 = 0
+if numbers == 0:
+    count0 += 1
+
+len = len(numbers)
+for i in numbers:
+    sum += 1
+if len == sum:
+    count1 += 1

0ac77d528a364098f747f423ad659a7577d029f5
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2025-11-07 13:57:53 +0100

    Update l9cw1.php

--- PAI-3/lesson-9/l9cw1.php
+++ PAI-3/lesson-9/l9cw1.php
@@ -4,23 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <?php
+    $backGround = "white";
+        if (isset($_POST['send'])) {
+            $color = $_POST['color'];
+            $backGround = $_POST['color'];
+        }
+    ?>
+    <style>
+        body{
+            background-color: <?php echo $backGround; ?>;
+            line-height: 200%;
+        }
+    </style>
 </head>
 <body>
+    <h1><strong>Wybór koloru strony</strong></h1>
     <form method="post">
-        <label for="color">Wybierz kolor tła strony:</label><br>
+        <label>Wybierz kolor tła strony:<br></label>
         <select name="color">
             <option value="white">Biały</option>
             <option value="red">Czerwony</option>
             <option value="blue">Niebieski</option>
             <option value="green">Zielony</option>
             <option value="pink">Różowy</option>
-            <option value="white">Biały</option>
-        </select><br>
-
-        <input type="submit" name="send" value="Zmień kolor strony">
+            <option value="orange">Pomarańczowy</option>
+        </select>
+        <br>
+        <input type="submit" name="send" value="Zmień kolor strony"></input>
     </form>
-    <?php
-        if(isset( $_POST['send']))
-    ?>
+    
 </body>
 </html>

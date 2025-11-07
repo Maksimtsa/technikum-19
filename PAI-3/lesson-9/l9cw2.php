5c93271bc3784d0d58bf61577741c5806884b1c4
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2025-11-07 14:10:19 +0100

    Add HTML form for basic arithmetic operations

---
+++ PAI-3/lesson-9/l9cw2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <label for="num1">Podaj 1 liczbę: <br></label>
+        <input type="number" id="num1"><br>
+        <label for="num2">Podaj 2 liczbę: <br></label>
+        <input type="number" id="num2"><br>
+        <label>Wybierz działanie:</label><br>
+        <input type="radio" id="+">
+        <label for="+">+</label><br>
+        <input type="radio" id="-">
+        <label for="-">-</label><br>
+        <input type="radio" id="*">
+        <label for="*">*</label><br>
+        <input type="radio" id="/">
+        <label for="/">/</label><br>
+        
+    </form>
+    <?php
+    
+    ?>
+</body>
+</html>

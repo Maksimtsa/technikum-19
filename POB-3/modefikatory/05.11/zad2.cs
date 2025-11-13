310b133ecbe7a3275aba2613a92f3311fdb5c451
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2025-11-13 10:10:27 +0100

    Create zad2.cs

---
+++ POB-3/modefikatory/05.11/zad2.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace zad1
+{
+    public class Uczen
+    {
+        public string Imie { get; private set; }
+        public string Nazwisko { get; private set; }
+        public List<int> Oceny { get; private set; }
+
+        public Uczen(string imie, string nazwisko)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Oceny = new List<int>();
+        }
+
+        public void DodajOcene(int ocena)
+        {
+            if (ocena >= 1 && ocena <= 6)
+                Oceny.Add(ocena);
+            else
+                Console.WriteLine($"Nieprawidłowa ocena: {ocena} (dozwolone 1–6).");
+        }
+
+        public double Srednia()
+        {
+            if (Oceny.Count == 0)
+                return 0;
+            return Oceny.Average();
+        }
+
+        public override string ToString()
+        {
+            return $"{Imie} {Nazwisko} – Średnia: {Srednia()}";
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Uczen> uczniowie = new List<Uczen>
+            {
+                new Uczen("Jan", "Kowalski"),
+                new Uczen("Anna", "Dudzik"),
+                new Uczen("Olaf", "Grygiel")
+            };
+
+            uczniowie[0].DodajOcene(5);
+            uczniowie[0].DodajOcene(4);
+            uczniowie[0].DodajOcene(6);
+            uczniowie[1].DodajOcene(6);
+            uczniowie[1].DodajOcene(5);
+            uczniowie[2].DodajOcene(3);
+            uczniowie[2].DodajOcene(4);
+
+            foreach (var u in uczniowie)
+            {
+                Console.WriteLine(u);
+            }
+        }
+    }
+}

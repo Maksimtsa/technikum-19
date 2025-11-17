80584499d42f6dc692bbdb789a9f7dff31a7d024
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2025-11-17 07:49:28 +0100

    Update zad1.cs

--- PAD/pictureBox/zad1.cs
+++ PAD/pictureBox/zad1.cs
@@ -1,1 +1,28 @@
+namespace _17_11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox.SelectedItem.ToString() == "koło")
+            {
+                pictureBox1.Image = Image.FromFile(@"obrazy\kolo.png");
+            }
+            if (comboBox.SelectedItem.ToString() == "trójkąt")
+            {
+                pictureBox1.Image = Image.FromFile(@"obrazy\trojkat.png");
+            }
+            if (comboBox.SelectedItem.ToString() == "prostokąt")
+            {
+                pictureBox1.Image = Image.FromFile(@"obrazy\prostokat.png");
+            }
+        }
+    }
+}
 

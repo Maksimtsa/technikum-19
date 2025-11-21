6d1352ff89c59f3549094d27e7762d385936aed8
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2025-11-21 14:05:15 +0100

    Add files via upload

---
+++ PAI-3/lesson-9/l9_2cw1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Formularz komentarza</title>
+</head>
+<style>
+    body {
+        line-height: 200%;
+    }
+</style>
+
+<body>
+    <h2>Dodaj komentarz</h2>
+
+    <form method="post" action="">
+        <label>Pseudonim:</label> <br>
+        <input type="text" name="pseudonim"><br>
+        <label>Komentarz:</label> <br>
+        <textarea cols="40" rows="5" name="comm"></textarea>
+    </form>
+
+</body>
+
+</html>

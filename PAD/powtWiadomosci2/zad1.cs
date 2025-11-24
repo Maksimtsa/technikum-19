48b1be1cf1addc19fe27ce938e905e27a33b586b
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2025-11-24 07:34:09 +0100

    Implement cost calculation for selected services
    Calculate total cost based on selected services.

--- PAD/powtWiadomosci2/zad1.cs
+++ PAD/powtWiadomosci2/zad1.cs
@@ -1,1 +1,51 @@
+using System.Windows.Forms;
 
+namespace powt1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int result = 0;
+            foreach (var option in checkedListBox.CheckedItems)
+            {
+                string opt = option.ToString();
+                if (opt == "Wymiana oleju 300 zł")
+                {
+                    result += 300;
+                }
+                else if (opt == "Zmiana opon 100 zł")
+                {
+                    result += 100;
+                }
+                else if(opt == "Wymiana klocków hamulcowych 400 zł")
+                {
+                    result += 400;
+                }
+                else if(opt == "Sprawdzenie klimatyzacji 120 zł")
+                {
+                    result += 120;
+                }
+                else if(opt == "Diagnostyka komputerowa 90 zł")
+                {
+                    result += 90;
+                }
+
+                if(result == 0)
+                {
+                    MessageBox.Show("Nic nie wybrano");
+                }
+                else
+                {
+                    textBox.Text = result.ToString();
+                }
+
+            }
+        }
+    }
+}

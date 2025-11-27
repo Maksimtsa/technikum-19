d95ce34e591919963d68b047e9e6060849eb9522
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2025-11-27 07:36:56 +0100

    Add files via upload

--- PAI-3/lesson-9/l9_2cw1.php
+++ PAI-3/lesson-9/l9_2cw1.php
@@ -4,23 +4,66 @@
 <head>
     <meta charset="UTF-8">
     <title>Formularz komentarza</title>
+    <style>
+        body {
+            line-height: 180%;
+            font-family: Arial;
+        }
+
+        .error {
+            color: red;
+            font-size: 14px;
+        }
+    </style>
 </head>
-<style>
-    body {
-        line-height: 200%;
-    }
-</style>
 
 <body>
+    <?php
+        $errP = $errComm = "";
+        $pseudonim = $komentarz = "";
+
+        if (isset($_POST['send'])) {
+
+            if (empty($_POST["pseudonim"])) {
+                $errP = "Uzupełnij pole";
+            } else {
+                $pseudonim = $_POST["pseudonim"];
+            }
+
+            if (empty($_POST["comm"])) {
+                $errComm = "Uzupełnij pole";
+            } else {
+                $komentarz = $_POST["comm"];
+            }
+        }
+?>
+
     <h2>Dodaj komentarz</h2>
 
-    <form method="post" action="">
-        <label>Pseudonim:</label> <br>
-        <input type="text" name="pseudonim"><br>
-        <label>Komentarz:</label> <br>
-        <textarea cols="40" rows="5" name="comm"></textarea>
-    </form>
+    <form method="post">
+        <label>Pseudonim:</label><br>
+        <input type="text" name="pseudonim">
+        <span class="error">
+            <?php echo $errP; ?>
+        </span>
+        <br><br>
 
+        <label>Komentarz:</label><br>
+        <textarea cols="40" rows="5" name="comm"></textarea>
+        <span class="error"><?php echo $errComm; ?></span>
+        <br>
+
+        <input type="submit" name="send" value="Wyślij">
+    </form>
+        <?php
+        if (isset($_POST['send'])){
+            if($errComm == "" && $errP == ""){
+                echo "Pseudonim: $pseudonim<br>";
+                echo "Komentarz: <strong>$komentarz</strong><br>";
+                echo "Komentarz: " . htmlspecialchars($komentarz);
+            }
+        }
+        ?>
 </body>
 
 </html>

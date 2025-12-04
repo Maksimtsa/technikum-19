dd3e7ab50cb6959c23c283c9231c701671f41e0f
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2025-12-04 10:16:36 +0100

    Update cw4.cs

--- POB-3/ToString/cw4.cs
+++ POB-3/ToString/cw4.cs
@@ -1,1 +1,49 @@
 
+using System;
+using System.Collections.Generic;
+
+namespace zad
+{
+    internal class Program
+    {
+        interface IGeometry
+        {
+            double Pole();
+        }
+
+        public abstract class Figura : IGeometry
+        {
+            public abstract double Pole();
+            public abstract string Opis();
+
+            public override string ToString()
+            {
+                return Opis();
+            }
+        }
+
+        public class Kolo : Figura
+        {
+            public int Radius { get; set; }
+            public Kolo(int radius)
+            {
+                Radius = radius;
+                
+            }
+            public override double Pole()
+            {
+                double pole = Math.Pow(Radius, 2) * 3.14;
+                return pole;
+            }
+            public override string Opis()
+            {
+                return $"Pole koła {pole}, radius {Radius}";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}

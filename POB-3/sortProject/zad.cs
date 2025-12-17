391b6984146c00524bb22a4b48d1f9e676d2ab10
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2025-12-17 18:34:52 +0100

    Update zad.cs

--- POB-3/sortProject/zad.cs
+++ POB-3/sortProject/zad.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 
 public class Program
 {
-
     public static void Main()
     {
         var colors = new string[] { "Pik", "Kier", "Karo", "Trefl" };
-        var numbers = new string[] {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+        var numbers = new string[] { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
 
         var cards = new List<string>();
+        var rnd = new Random();
 
         foreach (var color in colors)
         {
@@ -17,6 +17,14 @@ public class Program
             {
                 cards.Add($"{color} {number}");
             }
+        }
+
+        for (int i = cards.Count - 1; i > 0; i--)
+        {
+            int j = rnd.Next(i + 1);
+            string tmp = cards[i];
+            cards[i] = cards[j];
+            cards[j] = tmp;
         }
 
         foreach (var card in cards)

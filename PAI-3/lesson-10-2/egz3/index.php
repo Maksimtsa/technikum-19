34ea6338992584ad2ca3a0dcabcd9a086119672c
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2026-03-27 14:07:53 +0100

    Add files via upload

---
+++ PAI-3/lesson-10-2/egz3/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forum o psach</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+
+<body>
+    <header>
+        <h1>Forum miłośników psów</h1>
+    </header>
+    <main>
+        <div class="blockLeft">
+            <img src="Avatar.png" alt="Użytkownik forum">
+            <video src="video.mp4" controls loop></video>
+        </div>
+        <div class="blockRigth"></div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+
+</html>

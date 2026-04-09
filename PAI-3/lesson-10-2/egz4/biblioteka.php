876b7a5bb4a3fab274b28cb082fba1aeee34e61b
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2026-04-09 07:43:55 +0100

    Add files via upload

---
+++ PAI-3/lesson-10-2/egz4/biblioteka.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteka publiczna</title>
+    <link rel="stylesheet" href="styl.css">
+</head>
+
+<body>
+    <header>
+        <h1>Biblioteka w Książkowicach Wielkich</h1>
+    </header>
+    <main>
+        <div class="leftBlock">
+            <h3>Polecamy działa autorów.</h1>
+                <ol>
+                    <li></li>
+                </ol>
+        </div>
+        <div class="centralBlock">
+            <h3>ul. Czytelnicza 25, Książkowice&nbsp;Wielkie</h3>
+            <a href="mailto:sekretariat@biblioteka.pl">Napisz do nas</a>
+            <img src="biblioteka.png" alt="książki">
+        </div>
+        <div class="rightBlock">
+            <div class="rightTop">
+                <h3>
+                    Dodaj czytelnika
+                </h3>
+                <form method="post">
+                    <label>Imię: <input type="text" name="name"></label><br>
+                    <label>Nazwisko: <input type="text" name="surname"></label><br>
+                    <label>Symbol: <input type="number" name="symbol"></label><br>
+                    <button type="submit" name="send">DODAJ</button>
+                </form>
+            </div>
+            <div class="rightBottom"></div>
+        </div>
+    </main>
+    <footer>
+        <p>Projekt strony: 111222333444</p>
+    </footer>
+</body>
+
+</html>

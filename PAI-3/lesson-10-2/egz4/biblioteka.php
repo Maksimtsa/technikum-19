7d717053524005edd0dfb1e43f3b5eed2cc06ece
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2026-04-10 12:46:37 +0100

    Add files via upload

--- PAI-3/lesson-10-2/egz4/biblioteka.php
+++ PAI-3/lesson-10-2/egz4/biblioteka.php
@@ -15,13 +15,29 @@
     <main>
         <div class="leftBlock">
             <h3>Polecamy działa autorów.</h1>
-                <ol>
-                    <li></li>
-                </ol>
+                <?php
+                    $serwer = 'localhost';
+                    $user = 'root';
+                    $password = '';
+                    $db = 'biblioteka';
+
+                    $mysql = mysqli_connect($serwer, $user, $password, $db);
+
+                    $query = mysqli_query($mysql, "SELECT autorzy.imie, autorzy.nazwisko 
+                                            FROM autorzy
+                                            ORDER BY autorzy.nazwisko ASC");
+
+                    echo "<ol>";
+                    while($row = mysqli_fetch_array($query)){
+                        echo "<li>$row[0] $row[1]</li>";
+                    }
+                    echo "</ol>";
+                ?>
+                
         </div>
         <div class="centralBlock">
             <h3>ul. Czytelnicza 25, Książkowice&nbsp;Wielkie</h3>
-            <a href="mailto:sekretariat@biblioteka.pl">Napisz do nas</a>
+            <a href="mailto:sekretariat@biblioteka.pl">Napisz do nas</a><br>
             <img src="biblioteka.png" alt="książki">
         </div>
         <div class="rightBlock">
@@ -35,13 +51,41 @@
                     <label>Symbol: <input type="number" name="symbol"></label><br>
                     <button type="submit" name="send">DODAJ</button>
                 </form>
+                <?php
+                    $name = '';
+                    $surname = '';
+                    $code = '';
+                    $result = '';
+
+                    if(isset($_POST['send'])){
+                        if(!empty($_POST['name']) && !empty($_POST['surname']) && !empty($_POST['symbol'])){
+                            $name = $_POST['name'];
+                            $surname = $_POST['surname'];
+                            $code = $_POST['symbol'];
+                            
+                            $result = "Czytelnik $name $surname został(a) dodany do bazy danych.";
+                            $query1 = mysqli_query($mysql, "INSERT INTO czytelnicy VALUES(NULL, '$name', '$surname', '$code')");
+                        }
+                        else{
+                            $result = "Wypełnij wszystkie pola.";
+                        }
+                    }
+                    
+                ?>
             </div>
-            <div class="rightBottom"></div>
+            <div class="rightBottom">
+                <?php
+                    echo "<p>$result</p>";
+                ?>
+            </div>
         </div>
     </main>
     <footer>
         <p>Projekt strony: 111222333444</p>
     </footer>
+    <?php
+        mysqli_close($mysql);  
+    ?>
 </body>
 
 </html>

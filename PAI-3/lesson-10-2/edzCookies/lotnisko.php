d22e8440d4345183df185a022150695b4f0ba935
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2026-05-21 07:42:20 +0100

    Add files via upload

---
+++ PAI-3/lesson-10-2/edzCookies/lotnisko.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Port Lotniczy</title>
+    <link rel="stylesheet" href="styl5.css">
+</head>
+
+<body>
+    <header>
+        <div><img src="zad5.png" alt="logo lotnisko"></div>
+        <div>
+            <h1>Przyloty</h1>
+        </div>
+        <div>
+            <h3>
+                Przydatne linki
+            </h3>
+            <a src="kwerendy.txt">Pobierz</a>
+        </div>
+    </header>
+    <main>
+    </main>
+    <footer>
+        <div></div>
+        <div></div>
+    </footer>
+</body>
+
+</html>

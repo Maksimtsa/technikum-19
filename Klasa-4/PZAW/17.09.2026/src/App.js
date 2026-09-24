f684954c5afb088f842d6fabb2ccaed590271d3a
Maksim <144340235+Maksimtsa@users.noreply.github.com>
2026-09-24 15:56:13 +0200

    Add files via upload

--- Klasa-4/PZAW/17.09.2026/src/App.js
+++ Klasa-4/PZAW/17.09.2026/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 const bryly = [
   {
     id: 0,
-    nazwa: "sześcian",
+    nazwa: "SZEŚCIAN",
     obraz: "/obrazy/szescian.png",
     wzoryPole: "P = 6 · a²",
     wzoryObjetosc: "V = a³",
@@ -15,7 +15,7 @@ const bryly = [
   },
   {
     id: 1,
-    nazwa: "prostopadłościan",
+    nazwa: "PROSTOPADŁOŚCIAN",
     obraz: "/obrazy/prostopadloscian.png",
     wzoryPole: "P = 2ab + 2ah + 2bh",
     wzoryObjetosc: "V = a · b · h",
@@ -29,7 +29,7 @@ const bryly = [
   },
   {
     id: 2,
-    nazwa: "kula",
+    nazwa: "KULA",
     obraz: "/obrazy/kula.png",
     wzoryPole: "P = 4 · π · r²",
     wzoryObjetosc: "V = 4/3 · π · r³",
@@ -43,26 +43,26 @@ const bryly = [
 
 function ObliczPole(bryly){
   const w = bryly.wymiary;
-  if(bryly.nazwa == "sześcian"){
+  if(bryly.nazwa == "SZEŚCIAN"){
     return 6 * Math.pow(w.a, 2);
   }
-  else if(bryly.nazwa == "prostopadłościan"){
+  else if(bryly.nazwa == "PROSTOPADŁOŚCIAN"){
     return 2 * w.c * w.b + 2 * w.c * w.h + 2 * w.h * w.b;
   }
-  else if(bryly.nazwa == "kula"){
+  else if(bryly.nazwa == "KULA"){
     return 4 * Math.PI * Math.pow(w.r, 2);
   }
 }
 
 function OblicObjetosc(bryly) {
   const w = bryly.wymiary;
-  if (bryly.nazwa === "sześcian") {
+  if (bryly.nazwa === "SZEŚCIAN") {
     return Math.pow(w.a, 3);
   } 
-  else if (bryly.nazwa === "prostopadłościan") {
+  else if (bryly.nazwa === "PROSTOPADŁOŚCIAN") {
     return w.c * w.b *w.h;
   } 
-  else if (bryly.nazwa === "kula") {
+  else if (bryly.nazwa === "KULA") {
     return (4 / 3) * Math.PI * Math.pow(w.r, 3);
   }
 }
@@ -71,44 +71,49 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <table>
+        <h1>Bryły</h1>
+      </header>
+      <div className='tableBlock'>
+        <table align='center'>
           <thead>
-            <tr>
-              <th>BRYŁA</th>
-              <th>ILUSTRACJA</th>
-              <th>WZORY</th>
-              <th>PRZYKŁAD</th>
+            <tr className='figury'>
+              <th><h3>BRYŁA</h3></th>
+              <th><h3>ILUSTRACJA</h3></th>
+              <th><h3>WZORY</h3></th>
+              <th><h3>PRZYKŁAD</h3></th>
             </tr>
           </thead>
 
           <tbody>
             {bryly.map((figura) => (
               <tr key={figura.id}>
-                <td>{figura.nazwa}</td>
+                <td className='poleNazwa'>{figura.nazwa}</td>
 
                 <td>
                   <img src={figura.obraz} alt={figura.nazwa} />
                 </td>
 
-                <td>
-                  <div>{figura.wzoryPole}</div>
-                  <div>{figura.wzoryObjetosc}</div>
+                <td className='poleObjet'>
+                  <div>Pole: {figura.wzoryPole}</div>
+                  <div>Objetosc: {figura.wzoryObjetosc}</div>
                 </td>
 
                 <td>
-                      {figura.wymiary.a && <div>a = {figura.wymiary.a}</div>}
-                      {figura.wymiary.c && <div>c = {figura.wymiary.c}</div>}
-                      {figura.wymiary.b && <div>b = {figura.wymiary.b}</div>}
-                      {figura.wymiary.h && <div>h = {figura.wymiary.h}</div>}
-                      {figura.wymiary.r && <div>r = {figura.wymiary.r}</div>}               
-                <p>POLE:{ObliczPole(figura)}</p>
-                <p>OBJETOSC:{OblicObjetosc(figura)}</p>
+                  <ol>
+                      {figura.wymiary.a && <li>a = {figura.wymiary.a}</li>}
+                      {figura.wymiary.c && <li>c = {figura.wymiary.c}</li>}
+                      {figura.wymiary.b && <li>b = {figura.wymiary.b}</li>}
+                      {figura.wymiary.h && <li>h = {figura.wymiary.h}</li>}
+                      {figura.wymiary.r && <li>r = {figura.wymiary.r}</li>}   
+                  </ol>            
+                <p className='Pole'>POLE:{ObliczPole(figura)}</p>
+                <p className='Objetosc'>OBJETOSC:{OblicObjetosc(figura)}</p>
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
-      </header>
+      </div>
     </div>
   );
 }
